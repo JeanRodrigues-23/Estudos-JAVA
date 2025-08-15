cc7b92a477fc97407c0fe7bc8a1f8087251c7812
JeanRodrigues-23 <jeantrabalhodf@gmail.com>
2025-08-15 16:28:31 -0300

    Feito novas tarefas

---
+++ Formação Java Web - Aplicações Com Spring Boot/screenmatch/src/main/java/br/com/alura/screenmatch/Principal/Principal.java
@@ -0,0 +1,36 @@
+package br.com.alura.screenmatch.Principal;
+
+import br.com.alura.screenmatch.model.DadosSerie;
+import br.com.alura.screenmatch.model.DadosTemporada;
+import br.com.alura.screenmatch.service.ConsumoAPI;
+import br.com.alura.screenmatch.service.ConverteDados;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Principal {
+    private Scanner leitura = new Scanner(System.in);
+    private ConsumoAPI consumoAPI = new ConsumoAPI();
+    private ConverteDados converteDados = new ConverteDados();
+    private final String ENDERECO = "https://www.omdbapi.com/?t=";
+    private final String API_KEY = "&apikey=fa1639af";
+
+    public void exibirMenu() {
+        System.out.println("Digite o nome da série para busca:");
+        String nomeSerie = leitura.nextLine();
+
+        String jsonSerie = consumoAPI.obterDados((ENDERECO + nomeSerie.replace(" ", "+")+API_KEY));
+        DadosSerie serie = converteDados.obterDados(jsonSerie, DadosSerie.class);
+        System.out.println(serie);
+
+        List<DadosTemporada> listaTemporadas = new ArrayList<>();
+		for (int i = 1; i <= serie.totalTemporadas(); i++) {
+			String jsonTemporada = consumoAPI.obterDados("https://www.omdbapi.com/?t=" + nomeSerie.replace(" ", "+") + "&season=" + i + "&apikey=fa1639af");
+			DadosTemporada temporada = converteDados.obterDados(jsonTemporada, DadosTemporada.class);
+			listaTemporadas.add(temporada);
+		}
+		listaTemporadas.forEach(System.out::println);
+    }
+
+}
--- Formação Java Web - Aplicações Com Spring Boot/screenmatch/src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
+++ Formação Java Web - Aplicações Com Spring Boot/screenmatch/src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
@@ -1,11 +1,17 @@
 package br.com.alura.screenmatch;
 
+import br.com.alura.screenmatch.Principal.Principal;
+import br.com.alura.screenmatch.model.DadosEpisodio;
 import br.com.alura.screenmatch.model.DadosSerie;
+import br.com.alura.screenmatch.model.DadosTemporada;
 import br.com.alura.screenmatch.service.ConsumoAPI;
 import br.com.alura.screenmatch.service.ConverteDados;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootApplication
 public class ScreenmatchApplication implements CommandLineRunner {
@@ -16,11 +22,26 @@ public class ScreenmatchApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		ConsumoAPI consumoAPI = new ConsumoAPI();
-		String json = consumoAPI.obterDados("https://www.omdbapi.com/?t=gilmore+girls&apikey=fa1639af");
-		System.out.println(json);
-		ConverteDados conversor = new ConverteDados();
-		DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
-		System.out.println(dados);
+//		ConsumoAPI consumoAPI = new ConsumoAPI();
+//		String jsonSerie = consumoAPI.obterDados("https://www.omdbapi.com/?t=gilmore+girls&apikey=fa1639af");
+//
+//		ConverteDados conversor = new ConverteDados();
+//		DadosSerie dados = conversor.obterDados(jsonSerie, DadosSerie.class);
+//		System.out.println(dados);
+//
+//		String jsonEpisodioSerie = consumoAPI.obterDados("https://www.omdbapi.com/?t=gilmore+girls&season=1&episode=8&apikey=fa1639af");
+//		DadosEpisodio episodio = conversor.obterDados(jsonEpisodioSerie, DadosEpisodio.class);
+//		System.out.println(episodio);
+//
+//		List<DadosTemporada> listaTemporadas = new ArrayList<>();
+//		for (int i = 1; i <= dados.totalTemporadas(); i++) {
+//			String jsonTemporada = consumoAPI.obterDados("https://www.omdbapi.com/?t=gilmore+girls&season=" + i + "&apikey=fa1639af");
+//			DadosTemporada temporada = conversor.obterDados(jsonTemporada, DadosTemporada.class);
+//			listaTemporadas.add(temporada);
+//		}
+//		listaTemporadas.forEach(System.out::println);
+
+		Principal principal = new Principal();
+		principal.exibirMenu();
 	}
 }
