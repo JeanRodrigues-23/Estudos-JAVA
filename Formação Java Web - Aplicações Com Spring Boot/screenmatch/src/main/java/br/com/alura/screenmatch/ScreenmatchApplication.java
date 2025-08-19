9017f04b5185d37a36c195fa1b66cc2e55b87177
JeanRodrigues-23 <jeantrabalhodf@gmail.com>
2025-08-19 00:04:58 -0300

    Feito novas tarefas

--- Formação Java Web - Aplicações Com Spring Boot/screenmatch/src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
+++ Formação Java Web - Aplicações Com Spring Boot/screenmatch/src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
@@ -22,24 +22,6 @@ public class ScreenmatchApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-//		ConsumoAPI consumoAPI = new ConsumoAPI();
-//		String jsonSerie = consumoAPI.obterDados("https://www.omdbapi.com/?t=gilmore+girls&apikey=fa1639af");
-//
-//		ConverteDados conversor = new ConverteDados();
-//		DadosSerie dados = conversor.obterDados(jsonSerie, DadosSerie.class);
-//		System.out.println(dados);
-//
-//		String jsonEpisodioSerie = consumoAPI.obterDados("https://www.omdbapi.com/?t=gilmore+girls&season=1&episode=8&apikey=fa1639af");
-//		DadosEpisodio episodio = conversor.obterDados(jsonEpisodioSerie, DadosEpisodio.class);
-//		System.out.println(episodio);
-//
-//		List<DadosTemporada> listaTemporadas = new ArrayList<>();
-//		for (int i = 1; i <= dados.totalTemporadas(); i++) {
-//			String jsonTemporada = consumoAPI.obterDados("https://www.omdbapi.com/?t=gilmore+girls&season=" + i + "&apikey=fa1639af");
-//			DadosTemporada temporada = conversor.obterDados(jsonTemporada, DadosTemporada.class);
-//			listaTemporadas.add(temporada);
-//		}
-//		listaTemporadas.forEach(System.out::println);
 
 		Principal principal = new Principal();
 		principal.exibirMenu();
