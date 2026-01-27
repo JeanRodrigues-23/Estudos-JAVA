a24fe599d60c5bd0eae0cb1fa0881d4b19a209f9
JeanRodrigues-23 <jeantrabalhodf@gmail.com>
2026-01-27 00:36:55 -0300

    Feito novas tarefas

--- Formação Java Web - Aplicações Com Spring Boot/java-exceptions-projeto_inicial/src/main/java/adopet/api/service/AdocaoService.java
+++ Formação Java Web - Aplicações Com Spring Boot/java-exceptions-projeto_inicial/src/main/java/adopet/api/service/AdocaoService.java
@@ -8,7 +8,6 @@ import adopet.api.model.Tutor;
 import adopet.api.repository.AdocaoRepository;
 import adopet.api.repository.PetRepository;
 import adopet.api.repository.TutorRepository;
-import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -39,17 +38,30 @@ public class AdocaoService {
         Pet pet = petRepository.getReferenceById(dto.idPet());
         Tutor tutor = tutorRepository.getReferenceById(dto.idTutor());
 
+        //Pet já adotado
+        if (pet.getAdotado()) {
+            throw new IllegalStateException("Pet já adotado");
+        }
+
+        //Pet com solicitação de adoção em andamento
+        Boolean petAdocaoEmAndamento = adocaoRepository.existsByPetIdAndStatus(dto.idPet(), StatusAdocao.AGUARDANDO_AVALIACAO);
+        if (petAdocaoEmAndamento) {
+            throw new UnsupportedOperationException("Pet com adoção em andamento");
+        }
+
+        //Tutor com 2 adoções aprovadas
+        Integer tutorAdocoes = adocaoRepository.countByTutorIdAndStatus(dto.idTutor(), StatusAdocao.APROVADO);
+        if (tutorAdocoes == 2) {
+            throw new IllegalStateException("Tutor com máximo de adoções");
+        }
+
         adocaoRepository.save(new Adocao(tutor,pet, dto.motivo()));
     }
 
     public void aprovar(AprovarAdocaoDTO dto){
         Adocao adocao = adocaoRepository.getReferenceById(dto.idAdocao());
-        try {
             adocao.marcarComoAprovada();
             adocao.getPet().marcarComoAdotado();
-        } catch (EntityNotFoundException e) {
-            System.out.println("Adoção não encontrada.");
-        }
     }
 
     public void reprovar(ReprovarAdocaoDTO dto){
